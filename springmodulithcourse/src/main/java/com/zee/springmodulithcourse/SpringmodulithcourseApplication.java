7ff981a07bd769fa6eb7106ca22b28385a4e0dff
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-01-03 11:45:33 +0100

    Lesson 5

---
+++ springmodulithcourse/src/main/java/com/zee/springmodulithcourse/SpringmodulithcourseApplication.java
@@ -0,0 +1,13 @@
+package com.zee.springmodulithcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmodulithcourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmodulithcourseApplication.class, args);
+	}
+
+}
