ae99a0c118208f30e11667200ca2d9fcca0d00c9
MARCHINO Alessandro 2154 <alessandro.marchino@csi.it>
2025-01-13 09:08:45 +0100

    Lesson 19

--- springmodulithcourse/src/main/java/com/zee/springmodulithcourse/order/OrderService.java
+++ springmodulithcourse/src/main/java/com/zee/springmodulithcourse/order/OrderService.java
@@ -1,23 +1,75 @@
 package com.zee.springmodulithcourse.order;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.stereotype.Service;
 
+import com.zee.springmodulithcourse.inventory.exposed.InventoryDto;
 import com.zee.springmodulithcourse.inventory.exposed.InventoryService;
+import com.zee.springmodulithcourse.order.dto.InventoryRequestDto;
 import com.zee.springmodulithcourse.order.dto.OrderDto;
 import com.zee.springmodulithcourse.order.dto.OrderResponseDto;
+import com.zee.springmodulithcourse.order.type.Status;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderService {
 
-	@SuppressWarnings("unused")
 	private final InventoryService inventoryService;
+	private final OrderRepository orderRepository;
 
-	public OrderResponseDto createOrder(OrderDto order) {
-		// TODO: get the inventories by names
-		// build and persist the order
+	public OrderResponseDto createOrder(OrderDto orderDto) {
+		// Get inventories by name
+		List<String> inventoryNames = orderDto.inventoryRequestDtoList().stream()
+				.map(InventoryRequestDto::inventoryName)
+				.toList();
+		List<InventoryDto> inventories = inventoryService.fetchAllInName(inventoryNames);
+		// Persist the Order
+		Order order = buildAndPersistOrder(orderDto);
+		log.info("Order created: {}", order);
+		
+		// build and persist the OrderInventory
 		return null;
+	}
+
+	private Order buildAndPersistOrder(OrderDto orderDto) {
+		Order order = new Order();
+		order.setOrderIdentifier(UUID.randomUUID().toString());
+		order.setCustomerName(orderDto.customerName());
+		order.setCustomerEmail(orderDto.customerEmail());
+		order.setStatus(Status.COMPLETED);
+		
+		return orderRepository.save(order);
+	}
+	
+	private void buildAndPersistOrderInventory(Order order, List<InventoryDto> inventories, List<InventoryRequestDto> inventoryRequestDtoList, AtomicLong amount) {
+		List<InventoryDto> orderInventories = new ArrayList<>(inventories.size());
+		inventories.forEach(inventoryDto -> {
+			OrderInventory orderInventory = new OrderInventory();
+			InventoryRequestDto inventoryRequestDto = getInventoryRequestDtoByName(inventoryDto.name(), inventoryRequestDtoList);
+			orderInventory.setOrderId(order.getId());
+			orderInventory.setInventoryId(inventoryDto.id());
+			orderInventory.setQuantity(inventoryRequestDto.quantity());
+			long totalPrice = inventoryDto.price() * inventoryRequestDto.quantity();
+			orderInventory.setTotalQuantityPrice(totalPrice);
+			amount.addAndGet(totalPrice);
+			
+			orderInventories.add(inventoryDto);
+		});
+		
+		
+	}
+	private static InventoryRequestDto getInventoryRequestDtoByName(String inventoryName, List<InventoryRequestDto> inventoryRequestDtoList) {
+		return inventoryRequestDtoList.stream()
+				.filter(inv -> inv.inventoryName().equals(inventoryName))
+				.findFirst()
+				.orElse(null);
 	}
 }
